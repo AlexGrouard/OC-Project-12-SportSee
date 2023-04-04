7c5a17ad34e935fb13cd2b14cac3b6be7564c0c4
AlexFreyjr <alexfreyjr@gmail.com>
2023-04-04 17:32:49 +0200

    archi

---
+++ src/layout/Layout.ts
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import styles from "./Layout.module.scss"
+import Header from "./Header/Header"
+import Sidebar from "./Sidebar/Sidebar"
+
+function Layout(params: type) {
+	return (
+	)
+}
