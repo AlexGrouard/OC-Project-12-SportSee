1b08a79d44cdb5a5cf8778349ab0a508e49dd10d
AlexFreyjr <alexfreyjr@gmail.com>
2023-04-05 16:29:27 +0200

    modify ts to tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,8 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import styles from "./Layout.module.scss"
 import Header from "./Header/Header"
 import Sidebar from "./Sidebar/Sidebar"
+import Home from "page/Home/Home"
 
-function Layout(params: type) {
+function Layout() {
 	return (
+		<Router>
+			<Header />
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+			</Routes>
+		</Router>
 	)
 }
+
+export default Layout
